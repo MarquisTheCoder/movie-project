8a48e2da4689c34f6a6f4e0638587fef88476383
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-10-05 15:48:06 -0500

    autosave script

---
+++ js/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+ Creator: Deshawn Marquis, Williams,
+ GitHub Profile: https://github.com/MarquisTheCoder,
+ Date Created: 10/5/22,
+ Time Created: 3:35 PM,
+ File Name: keys.js,
+ File Description:
+ API keys for the SPA
+ */
