75e915e6ca5af31aaf289d553d4c92d79bf93765
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-10-06 09:12:04 -0500

    autosave script

--- js/main.js
+++ js/main.js
@@ -10,9 +10,14 @@
  */
 
 $(function(){
+     function getRandomArbitrary(min, max) {
+          return Math.random() * (max - min) + min;
+     }
+     
      async function getMovies(){
           let results = await fetch('https://clear-malleable-asparagus.glitch.me/movies')
           let movies = await results.json()
+          let max = movies.length
           
           for(let i = 0; i <= 7; i++){
                let movieBox = $(`<div class='generated-content--box' data-embedded='${movies[i].embeddedUrl}'> </div>`)
@@ -20,7 +25,29 @@ $(function(){
                movieBox.css('background', `url("${movies[i].imgLink}") no-repeat`)
                movieBox.css('background-size', 'cover');
                
-               $('#featured').append(movieBox); 
+               $('#featured').append(movieBox);
+          }
+          
+          for(let i = 0; i <= 7; i++){
+               let movieIndex = Math.floor(getRandomArbitrary(5,max-1))
+               console.log(movieIndex)
+               let movieBox = $(`<div class='generated-content--box' data-embedded='${movies[movieIndex].embeddedUrl}'> </div>`)
+               movieBox.css('position', 'relative')
+               movieBox.css('background', `url("${movies[movieIndex].imgLink}") no-repeat`)
+               movieBox.css('background-size', 'cover');
+          
+               $('#top-picks').append(movieBox);
+          }
+          
+          for(let i = 0; i <= 7; i++){
+               let movieIndex = Math.floor(getRandomArbitrary(5,max-1))
+               console.log(movieIndex)
+               let movieBox = $(`<div class='generated-content--box' data-embedded='${movies[movieIndex].embeddedUrl}'> </div>`)
+               movieBox.css('position', 'relative')
+               movieBox.css('background', `url("${movies[movieIndex].imgLink}") no-repeat`)
+               movieBox.css('background-size', 'cover');
+          
+               $('#favorites').append(movieBox);
           }
      }
      getMovies()
